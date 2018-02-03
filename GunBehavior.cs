353cbcc33dda469f842b1b40559d6678d97c3560
Petrillod0 <35899556+Petrillod0@users.noreply.github.com>
2018-02-03 00:12:02 -0500

    Add files via upload
    gunbehaviour script fires an object at the position of your mouse and the rotateObject script rotates an object to match the position of your mouse.

---
+++ GunBehavior.cs
@@ -0,0 +1,36 @@
+﻿using UnityEngine;
+
+public class GunBehavior : MonoBehaviour
+{
+
+    Vector3 mouse_pos;
+    public GameObject bullet;
+    public GameObject gun;
+    public float speed = 5.0f;
+
+    // Use this for initialization
+    void Start()
+    {
+
+    }
+
+    void Update()
+    {
+        if (Input.GetMouseButtonDown(0))
+        {
+            Vector3 target = Camera.main.ScreenToWorldPoint(new Vector2(Input.mousePosition.x, Input.mousePosition.y));
+            Vector3 direction = target - gun.transform.position;
+            direction.Normalize();
+            Quaternion rotation = Quaternion.Euler(0, 0, Mathf.Atan2(direction.y, direction.x) * Mathf.Rad2Deg);
+            GameObject projectile = (GameObject)Instantiate(bullet, gun.transform.position, rotation);
+            projectile.GetComponent<Rigidbody2D>().velocity = direction * speed;
+            
+        }
+    }
+}
+
+
+
+
+
+
