dd1d4b95677cca4f10d964523de58e0baeb52e5c
steinmetc8 <32173978+steinmetc8@users.noreply.github.com>
2018-02-18 19:47:29 -0500

    Add files via upload

---
+++ BulletBehavior.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BulletBehavior : MonoBehaviour {
+    double lifetime = 1.0;
+    public bool shrinkable;
+    
+    void OnCollisionEnter2D(Collision2D col)
+    {
+        if (col.gameObject.tag == "Shootable" && shrinkable)
+        {
+            col.transform.localScale *= .80f;
+        }
+        else if (col.gameObject.tag == "Shootable" && !shrinkable)
+        {
+            col.transform.localScale *= 1.05f;
+        }
+        Destroy(gameObject);
+    }
+	
+	// Update is called once per frame
+	void Update () {
+
+        lifetime -= 0.7 * Time.deltaTime;
+        if (lifetime <= 0.0)
+            Destroy(gameObject);
+
+    }
+}
--- GunBehavior.cs
+++ GunBehavior.cs
@@ -3,30 +3,34 @@
 public class GunBehavior : MonoBehaviour
 {
 
-    Vector3 mouse_pos;
-    public GameObject bullet;
+    public GameObject bulletModel;
     public GameObject gun;
     public float speed = 5.0f;
-
-    // Use this for initialization
-    void Start()
-    {
-
-    }
-
+    
     void Update()
     {
+        Vector3 target = Camera.main.ScreenToWorldPoint(new Vector2(Input.mousePosition.x, Input.mousePosition.y));
+        Vector3 direction = target - gun.transform.position;
+        direction.Normalize();
+        Quaternion rotation = Quaternion.Euler(0, 0, Mathf.Atan2(direction.y, direction.x) * Mathf.Rad2Deg);
+
         if (Input.GetMouseButtonDown(0))
         {
-            Vector3 target = Camera.main.ScreenToWorldPoint(new Vector2(Input.mousePosition.x, Input.mousePosition.y));
-            Vector3 direction = target - gun.transform.position;
-            direction.Normalize();
-            Quaternion rotation = Quaternion.Euler(0, 0, Mathf.Atan2(direction.y, direction.x) * Mathf.Rad2Deg);
-            GameObject projectile = (GameObject)Instantiate(bullet, gun.transform.position, rotation);
+            GameObject projectile = (GameObject)Instantiate(bulletModel, gun.transform.position, rotation);
+            projectile.AddComponent<BulletBehavior>().shrinkable = true;
             projectile.GetComponent<Rigidbody2D>().velocity = direction * speed;
-            
         }
+
+        if(Input.GetMouseButtonDown(1))
+        {
+            GameObject projectile = (GameObject)Instantiate(bulletModel, gun.transform.position, rotation);
+            projectile.AddComponent<BulletBehavior>().shrinkable = false;
+            projectile.GetComponent<Rigidbody2D>().velocity = direction * speed;
+        }
+
     }
+
+
 }
 
 
